2198b7dd1fbb1fdad626e06f26017e8ba60d6165
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-08-04 16:44:59 +0700

    selecting elements

--- script.ts
+++ script.ts
@@ -1,11 +1,26 @@
-let expenses: object = [];
+// Structuring empty expenses object
+interface Expense {
+  category: string;
+  date: string;
+  amount: number;
+}
+
+let expenses: Expense[] = [];
 let totalAmount: number = 0;
 
+// SELECTING ELEMENTS
 const categorySelect = document.getElementById(
   "category-select"
 ) as HTMLSelectElement;
 const dateInput = document.getElementById("date-input") as HTMLInputElement;
 const amountInput = document.getElementById("amount-input") as HTMLInputElement;
 const addBtn = document.getElementById("add-btn") as HTMLButtonElement;
-const expensesTableBody = document.getElementById("expense-table-body");
-const totalAmountCell = document.getElementById("total-amount");
+const expensesTableBody = document.getElementById(
+  "expense-table-body"
+) as HTMLTableSectionElement;
+const totalAmountCell = document.getElementById("total-amount") as HTMLElement;
+
+// BUTTON ADD EVENT CLICKED
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+});
--- script/script.js
+++ script/script.js
@@ -1,9 +1,14 @@
 "use strict";
 let expenses = [];
 let totalAmount = 0;
+// SELECTING ELEMENTS
 const categorySelect = document.getElementById("category-select");
 const dateInput = document.getElementById("date-input");
 const amountInput = document.getElementById("amount-input");
 const addBtn = document.getElementById("add-btn");
 const expensesTableBody = document.getElementById("expense-table-body");
 const totalAmountCell = document.getElementById("total-amount");
+// BUTTON ADD EVENT CLICKED
+addBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+});
