1a8e15aa0b6cc8ea0a887f3d5aa77840302c9ccb
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-08-04 17:41:09 +0700

    make delete btn event

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-// Structuring empty expenses object
+// STRUCTURING EMPTY EXPENSES OBJECT
 interface Expense {
   category: string;
   date: string;
@@ -23,4 +23,49 @@ const totalAmountCell = document.getElementById("total-amount") as HTMLElement;
 // BUTTON ADD EVENT CLICKED
 addBtn.addEventListener("click", function (e) {
   e.preventDefault();
+
+  const category: string = categorySelect.value;
+  const amount: number = Number(amountInput.value);
+  const date: string = dateInput.value;
+
+  if (amount <= 0) {
+    alert("Please enter the correct amount! ❌");
+    return;
+  }
+  if (date === "") {
+    alert("Choose your expense date 📅");
+    return;
+  }
+
+  // PUSH TO EXPENSES ARRAY
+  expenses.push({ category, date, amount });
+
+  // TOTAL THE AMOUNT & INSERT TO TOTAL CELL
+  totalAmount += amount;
+  totalAmountCell.textContent = totalAmount.toString();
+
+  // INSERT FORM CONTENT TO TABLE BODY
+  const newRow = expensesTableBody.insertRow();
+  const categoryCell = newRow.insertCell();
+  const dateCell = newRow.insertCell();
+  const amountCell = newRow.insertCell();
+  const deleteCell = newRow.insertCell();
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("delete-btn");
+
+  // DELETE BUTTON EVENT
+  deleteBtn.addEventListener("click", function () {
+    const rowIndex = newRow.rowIndex - 1;
+    const expense = expenses[rowIndex];
+
+    if (expense) {
+      expenses.splice(rowIndex, 1);
+
+      totalAmount -= expense.amount;
+      totalAmountCell.textContent = totalAmount.toString();
+      expensesTableBody.removeChild(newRow);
+    }
+  });
 });
--- script/script.js
+++ script/script.js
@@ -11,4 +11,40 @@ const totalAmountCell = document.getElementById("total-amount");
 // BUTTON ADD EVENT CLICKED
 addBtn.addEventListener("click", function (e) {
     e.preventDefault();
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
+    if (amount <= 0) {
+        alert("Please enter the correct amount! ❌");
+        return;
+    }
+    if (date === "") {
+        alert("Choose your expense date 📅");
+        return;
+    }
+    // PUSH TO EXPENSES ARRAY
+    expenses.push({ category, date, amount });
+    // TOTAL THE AMOUNT & INSERT TO TOTAL CELL
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount.toString();
+    // INSERT FORM CONTENT TO TABLE BODY
+    const newRow = expensesTableBody.insertRow();
+    const categoryCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("delete-btn");
+    // DELETE BUTTON EVENT
+    deleteBtn.addEventListener("click", function () {
+        const rowIndex = newRow.rowIndex - 1;
+        const expense = expenses[rowIndex];
+        if (expense) {
+            expenses.splice(rowIndex, 1);
+            totalAmount -= expense.amount;
+            totalAmountCell.textContent = totalAmount.toString();
+            expensesTableBody.removeChild(newRow);
+        }
+    });
 });
